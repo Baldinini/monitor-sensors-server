d1ba2d58fed87bac895e0e185e66efc0d3053316
Baldinini <baldinat063@gmail.com>
2021-03-22 12:54:16 +0200

    first commit

---
+++ src/main/java/com/example/monitorsensorsserver/MonitorSensorsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitorsensorsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorSensorsServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MonitorSensorsServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/monitorsensorsserver/MonitorSensorsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitorsensorsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSensorsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
